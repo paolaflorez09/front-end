f6dccd5771241d11e8911f7b906345372a423649
paolaflorez09 <laurapaolaflorez@gmail.com>
2021-12-11 15:07:47 -0500

    abaut

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,46 @@
 <template>
-  <div class="about">
-    <h1>hola  como esta</h1>
-  </div>
+<header class="titulo">
+               <h1>GUARDERIA CANINOS FELICES</h1>
+            </header>
+             
+            
+            
+            <nav >
+                
+                <ul>
+               
+                  <li><span><a href="principal.html">Principal</a></span></li>
+                  <li><span><a href="servicios.html">Servicios</a></span></li>
+                  <li><span><a href="contacto.html">Contacto</a></span></li>
+                  <li><span><a href="historia.html">Historia</a></span></li>
+                </ul>
+            </nav>
+            <h1>CONOCENOS</h1>
+
+                    <P>El proyecto GUARDERÍA CANINO FELICES. fue creada con la intensión de una propuesta
+                    de negocio que se trata de una guardería especializada para perros donde estos recibirán
+                    un cuidado y una enseñanza básica en caso que los dueños por algún motivo no puedan cuidar de ellos.
+                    También contamos con una tienda virtual de productos, Esta guardería contara con un valor 
+                    agregado que son las cámaras en todos los ambientes que permitirán poder observar a tu mascota en tiempo
+                    real desde donde te encuentres y de esta manera puedas saber cómo esta además de asegurarte de que reciba
+                    un buen cuidado y todo lo necesario para su bienestar.</P>
+
+                     <h1>MISION</h1> 
+                   
+                  “Lograr posicionarse en el mercado y ser la mejor tienda virtual reconocido 
+                  en el medio promoviendo el bienestar animal mediante, la prestación
+                  de nuestros servicios de cuidado médicos veterinarios y complementarios
+                  con los mejores productos para un excelente bienestar y fomentar en 
+                  las nuevas generaciones el gusto por este maravilloso cuidado sobre sus animales”
+                  
+                  
+                  <h1>VISION</h1> 
+
+
+                   consolidarnos como la empresa líder en la rama del Billar y Casino D`elite, reconocida por ofrecer un 
+                   concepto diferente en diversión, dándole un momento increíble al cliente, como que escojan ellos la 
+                   música que más le guste. Por media hora basándonos en la seguridad que ofrecemos a nuestros clientes.
+
 </template>
 
 <script>
